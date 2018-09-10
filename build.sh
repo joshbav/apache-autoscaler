6c330e50584121584da0479dadf19f97ecd1256f
“joshbav” <josh@Joshs-Mac.frontierlocal.net>
2018-09-09 21:26:36 -0700

    scripted commit 09-09-18

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-docker build --squash -t joshbav/apache-autoscaler:latest .
+# docker build --squash -t joshbav/apache-autoscaler:latest .
 echo
 echo
 echo
-echo Pushing newly built image to dockerhub
+#echo Pushing newly built image to dockerhub
 echo
-docker push joshbav/apache-autoscaler:latest
+#docker push joshbav/apache-autoscaler:latest
 echo
 echo
 # All files to automatically be added
